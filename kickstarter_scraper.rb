85bcb947d5d81cd5ab3a88f535a500888cb2d18b
Alan Tang <giabao_tang@yahoo.com>
2020-10-18 04:51:19 +0000

    Automatically backed up by Learn

--- kickstarter_scraper.rb
+++ kickstarter_scraper.rb
@@ -1,8 +1,11 @@
 # require libraries/modules here
 require 'nokogiri'
+requier 'pry'
 def create_project_hash
   # write your code here
   html = File.read('fixtures/kickstarter.html')
  
   kickstarter = Nokogiri::HTML(html)
+  binding.pry 
+
 end
